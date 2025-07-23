e50f1c69bf26bc6a1947f0b42ab6f56b190d552f
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-07-23 15:47:29 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- plugins/npm.js
+++ plugins/npm.js
@@ -33,7 +33,7 @@ cmd({
 
     // Create the response message
     const message = `
-*KHAN-MD NPM SEARCH*
+*CASEYRHODES-MD NPM SEARCH*
 
 *🔰 NPM PACKAGE:* ${packageName}
 *📄 DESCRIPTION:* ${description}
